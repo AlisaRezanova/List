6d051e1f84efce83515e94d3d6e8edf72962f918
4319 Резанова А.Р. <alsa2002g@gmail.com>
2025-01-06 00:34:15 +0300

    .

--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
 import requests
+def test_edit():
+    url = "http://127.0.0.1:8000/edit_title/2/"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "note": "This is an updated note.",
+        "rating": 8
+    }
 
-url = "http://127.0.0.1:8000/edit_title/2/"
-headers = {"Content-Type": "application/json"}
-data = {
-    "note": "This is an updated note.",
-    "rating": 8
-}
+    response = requests.post(url, json=data, headers=headers)
+    print(f"Status Code: {response.status_code}")
+    print(f"Response: {response.json()}")
 
-response = requests.post(url, json=data, headers=headers)
-print(f"Status Code: {response.status_code}")
-print(f"Response: {response.json()}")
