cd7b5cebb9154cbc5a31183e391e6dabb843983c
4319 Резанова А.Р. <alsa2002g@gmail.com>
2025-07-22 14:25:21 +0300

    add .env and edit settings.py

--- List/settings.py
+++ List/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +11,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-28tj7!hst9taw0_wk#cy2yxpw6x$8v(au+nepse=667hvdo=pc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', cast=bool, default=False)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
